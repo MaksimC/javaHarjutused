55d4dfa32c23cd9626734c4075ab5c8a7e03c894
Maksim Tšeljabov <mtseljab@klass3-06-l.intra.itcollege.ee>
2015-10-10 13:30:27 +0300

    praktikumi tulemus

--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -1,8 +1,38 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lib.Foor;
+
 /**
  * Täpselt nagu oli fooriga, on nüüd ristmikuga. Kuidas esiteks
  * ristmik üles ehitada loe Foori README.md-st: https://github.com/KristerV/javaHarjutused
  */
-public class Peamurdja2_ristmik {
-}
+public class Peamurdja2_ristmik  extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Foor foor1 = new Foor(Foor.YLEMINE, primaryStage);
+        foor1.vahetaPunast();
+        foor1.paus(0.5);
+
+        Foor foor2 = new Foor(Foor.ALUMINE, primaryStage);
+        foor2.vahetaPunast();
+        foor2.paus(0.5);
+        Foor foor3 = new Foor(Foor.PAREM, primaryStage);
+        foor3.vahetaPunast();
+        foor3.paus(0.5);
+        Foor foor4 = new Foor(Foor.VASAK, primaryStage);
+        foor4.vahetaPunast();
+        foor4.paus(0.5);
+
+        roheliseks (foor1);
+        roheliseks(foor1);
+        roheliseks(foor1);
+        roheliseks(foor1);
+
+        public void roheliseks(Foor misiganes) {
+
+    }
+    }
+    }
+
