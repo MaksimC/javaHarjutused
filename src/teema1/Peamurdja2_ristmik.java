3be310fb33ad71299972968bbe1f04a8b8517dea
Maksim Tšeljabov <mtseljab@klass1-28-l.intra.itcollege.ee>
2015-10-11 14:28:08 +0300

    ristmike optimaalne lahendus

--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -11,21 +11,81 @@ import lib.Foor;
 public class Peamurdja2_ristmik  extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Foor foor1 = new Foor(Foor.YLEMINE, primaryStage);
-        roheliseks(foor1);
-
+        Foor foorYlemine = new Foor(Foor.YLEMINE, primaryStage);
+        Foor foorAlumine = new Foor(Foor.ALUMINE, primaryStage);
+        Foor foorParem = new Foor(Foor.PAREM, primaryStage);
+        Foor foorVasak = new Foor(Foor.VASAK, primaryStage);
+        tsykkel(foorYlemine, 0);
+        tsykkel(foorAlumine, 0);
+        tsykkel(foorParem, 1);
+        tsykkel(foorVasak, 1);
     }
 
-    public void roheliseks(Foor misiganes) {
+    public void tsykkel (Foor foor, int suund) {
+        int count = suund;
+        while (count<10){
 
-        misiganes.vahetaPunast();
-        misiganes.paus(0.5);
-        misiganes.vahetaKollast();
-        misiganes.paus(0.5);
-        misiganes.vahetaPunast();
-        misiganes.vahetaKollast();
-        misiganes.vahetaRohelist();
+
+            if (suund !=0){
+                foor.vahetaPunast();
+                foor.paus(7.5);
+                foor.vahetaKollast();
+                foor.paus(0.5);
+                foor.vahetaPunast();
+                foor.vahetaKollast();
+
+            }
+                foor.vahetaRohelist();
+                foor.paus(3);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+
+                foor.vahetaKollast();
+                foor.paus(0.5);
+                foor.vahetaKollast();
+
+                foor.vahetaRohelist();
+                foor.vahetaRohelist();
+
+
+
+
+            }
+            /*else {
+                foor.vahetaRohelist();
+                foor.paus(3);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+                foor.paus(0.5);
+                foor.vahetaRohelist();
+
+                foor.vahetaKollast();
+                foor.paus(0.5);
+                foor.vahetaKollast();
+
+
+
+                foor.vahetaPunast();
+                foor.paus(7.5);
+                foor.vahetaKollast();
+                foor.paus(0.5);
+                foor.vahetaPunast();
+                foor.vahetaKollast();
+
+            }*/
+            count++;
 
     }
 }
-
