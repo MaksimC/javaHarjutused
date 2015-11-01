800f6d1544becaac51b1fe14b5024618025c730b
Maksim Cheljabov <maksim.tseljabov@gmail.com>
2015-11-01 20:25:53 +0200

    upd

--- src/teema2/Peamurdja1_laevad_fx.java
+++ src/teema2/Peamurdja1_laevad_fx.java
@@ -16,18 +16,18 @@ import javafx.stage.Stage;
  * Joonista laevade pommitamine kasutades JavaFXi.
  */
 public class Peamurdja1_laevad_fx extends Application {
-    Stage lava;
-    StackPane maailm;
-    GridPane laud;
-    int laevaServaPikkus = 150;
-    int lauaPikkus = 3;
-    Image laevaPilt = new Image("teema2/pirate.png");
-    ImagePattern laevaMuster = new ImagePattern(laevaPilt);
+    Stage stage;
+    StackPane world;
+    GridPane table;
+    int shipSize = 150;
+    int tableSize = 3;
+    Image shipPicture = new Image("teema2/pirate.png");
+    ImagePattern shipTemplate = new ImagePattern(shipPicture);
 
 
     @Override
     public void start (Stage primaryStage) throws Exception {
-        lava = primaryStage;
+        stage = primaryStage;
         seadistaLava();
         gereeriGrid();
         reageeriKlikidele();
@@ -36,40 +36,39 @@ public class Peamurdja1_laevad_fx extends Application {
     }
 
     private void seadistaLava() {
-        maailm = new StackPane();
-        laud = new GridPane();
-        maailm.getChildren().add(laud);
-        Scene manguStseen = new Scene (maailm, laevaServaPikkus*lauaPikkus, laevaServaPikkus*lauaPikkus);
-        lava.setScene(manguStseen);
-        lava.show(); //naita aken
-        lava.setOnCloseRequest(event -> {System.exit(0); }); //akna sulgedes laheb programm kinni
+        world = new StackPane();
+        table = new GridPane();
+        world.getChildren().add(table);
+        Scene manguStseen = new Scene (world, shipSize*tableSize, shipSize*tableSize);
+        stage.setScene(manguStseen);
+        stage.show(); //naita aken
+        stage.setOnCloseRequest(event -> {System.exit(0); }); //akna sulgedes laheb programm kinni
     }
 
     private void gereeriGrid() {
-        for (int i = 0; i < lauaPikkus; i++) {
-            for (int j = 0; j < lauaPikkus; j++) {
-                Rectangle ruut = new Rectangle(laevaServaPikkus, laevaServaPikkus);
+        for (int i = 0; i < tableSize; i++) {
+            for (int j = 0; j < tableSize; j++) {
+                Rectangle kvadrat = new Rectangle(shipSize, shipSize);
                 int rand = (int) (Math.random() * 2);
-                if (rand == 1) {
-                    ruut.setId("meri");
+                if (rand == 1) {kvadrat.setId("meri");
                 } else {
-                    ruut.setId("laev");
+                    kvadrat.setId("laev");
                 }
-                ruut.setFill(Color.BLUE);
-                ruut.setStroke(Color.BLACK);
-                laud.add (ruut, i, j);
+                kvadrat.setFill(Color.BLUE);
+                kvadrat.setStroke(Color.BLACK);
+                table.add (kvadrat, i, j);
             }
         }
     }
 
     private void gameover() {
         Label tekst = new Label ("tubli");
-        maailm.getChildren().add(tekst);
+        world.getChildren().add(tekst);
     }
 
     private boolean laevasidAlles() {
-        for (Node ruut : laud.getChildren()){
-            if (ruut.getId().equals ("laev")) {
+        for (Node kvadrat : table.getChildren()){
+            if (kvadrat.getId().equals ("laev")) {
                 return true;
             }
         }
@@ -77,16 +76,19 @@ public class Peamurdja1_laevad_fx extends Application {
     }
 
     private void reageeriKlikidele() {
-        laud.setOnMouseClicked(event -> {
+        table.setOnMouseClicked(event -> {
             Rectangle ruut = (Rectangle) event.getTarget();
             String ruutID = ruut.getId();
             if (ruutID.equals("laev")) {
-                ruut.setFill(Color.RED);
+                ruut.setFill(shipTemplate);
                 ruut.setId("pohjas");
             } else if (ruutID.equals("meri")) {
                 ruut.setFill(Color.DARKBLUE);
             } else if (ruutID.equals("pohjas")){
                 System.out.println("juba lasid sinna");
+            }
+            if (!laevasidAlles());{
+                gameover();
             }
         });
     }
